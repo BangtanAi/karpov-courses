eb647791d26eee8b442ba1919bd56b74c192cba0
Aidana Kairova <a.kairova@codecraft.kz>
2023-06-25 17:21:43 +0600

    KC-2 eslint, stylelint, prettier, webpack plugins for style, es -lint auto reformatting

--- src/Components/Navigation/Navigation.tsx
+++ src/Components/Navigation/Navigation.tsx
@@ -1,28 +1,28 @@
-import React, { FC } from 'react';
-import { categoryNames } from '../../utils';
-import './Navigation.css';
-import logo from '../../images/logo.svg';
+import React, { FC } from "react";
+import { categoryNames } from "../../utils";
+import "./Navigation.css";
+import logo from "../../images/logo.svg";
 
 interface Props {
-    onNavClick: (event: React.MouseEvent<HTMLElement>) => void;
-    currentCategory: string;
-    className?: string;
-    placement: 'header' | 'footer';
+  onNavClick: (event: React.MouseEvent<HTMLElement>) => void;
+  currentCategory: string;
+  className?: string;
+  placement: "header" | "footer";
 }
 
-export const Navigation: FC<Props> = ({ onNavClick, currentCategory, className = '', placement = 'header' }) => {
+export const Navigation: FC<Props> = ({ onNavClick, currentCategory, className = "", placement = "header" }) => {
   return (
     <nav className={`grid navigation navigation--${placement} ${className}`}>
       <a className="navigation__logo" data-href="index" href="#">
         <img className="navigation__logo-image" src={logo} alt="Логотип" />
       </a>
       <ul className="navigation__list">
-        {['index', 'fashion', 'technologies', 'sport', 'karpov'].map((item) => {
+        {["index", "fashion", "technologies", "sport", "karpov"].map((item) => {
           return (
             <li className="navigation__item" key={item}>
               <a
                 onClick={onNavClick}
-                className={`navigation__link ${currentCategory === item ? 'navigation__link--active' : '' }`}
+                className={`navigation__link ${currentCategory === item ? "navigation__link--active" : ""}`}
                 data-href={item}
                 href="#"
               >
@@ -30,9 +30,9 @@ export const Navigation: FC<Props> = ({ onNavClick, currentCategory, className =
                 {categoryNames[item]}
               </a>
             </li>
-          )
+          );
         })}
       </ul>
     </nav>
-  )
-}
+  );
+};
